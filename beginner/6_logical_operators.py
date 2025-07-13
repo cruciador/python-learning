40eb30973a54746dec9ce57053a4ec51eb08525f
Gaurav Maithani <gmaithani2014@gmail.com>
2025-07-13 18:00:07 +0530

    Added more notes for logical operations in python

--- beginner/6_logical_operators.py
+++ beginner/6_logical_operators.py
@@ -2,3 +2,22 @@ x = 10
 y = 20
 
 print ("not (x+y>15):", not (x+y)>15)
+
+'''Logical Operators To Compare Sequences (Lists)
+Finally, two list objects below are non-empty. Hence x and y returns the latter, and x or y returns the former.'''
+
+x=[1,2,3]
+y=[10,20,30]
+
+print("x  and  y: ", x and y)
+
+print("x  or  y:", x or y)
+
+# Logical operations with non boolean conditions
+x = 10
+y = 20
+z = 0
+print("x and y:",x and y)
+print("x or y:",x or y)
+print("z or x:",z or x)
+print("y or z:", y or z)
