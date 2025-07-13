872132389e3dbf672c5f8788071db1f1f61f16a0
Gaurav Maithani <gmaithani2014@gmail.com>
2025-07-13 11:09:54 +0530

    Data types in python

--- beginner/4_data_types.py
+++ beginner/4_data_types.py
@@ -18,3 +18,15 @@ print (str[2:])      # Prints string starting from 3rd character
 print (str * 2)      # Prints string two times
 print (str + "TEST") # Prints concatenated string
 
+# Example of List Data Type
+
+list = [ 'abcd', 786 , 2.23, 'john', 70.2 ]
+tinylist = [123, 'john']
+
+
+print (list)          # Prints complete list
+print (list[0])       # Prints first element of the list
+print (list[1:3])     # Prints elements starting from 2nd to
+print (list[2:])      # Prints elements starting from 3rd element
+print (tinylist * 2)  # Prints list two times
+print (list + tinylist) # Prints concatenated lists
