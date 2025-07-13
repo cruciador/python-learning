88653b597ff252e5674d2ea1e856a89881966bf4
Gaurav Maithani <gmaithani2014@gmail.com>
2025-07-13 11:03:26 +0530

    Add data types

---
+++ beginner/4_data_types.py
@@ -0,0 +1,20 @@
+## Remember, number objects are created when you assign a value to a variable.
+
+var1 = 1       # int data type
+var2 = True    # bool data type
+var3 = 10.023  # float data type
+var4 = 10+3j   # complex data type
+
+print(type(var4)) # <class 'complex'>
+
+# String data type
+
+str = 'Hello World!'
+
+print (str)          # Prints complete string
+print (str[0])       # Prints first character of the string
+print (str[2:5])     # Prints characters starting from 3rd to 5th
+print (str[2:])      # Prints string starting from 3rd character
+print (str * 2)      # Prints string two times
+print (str + "TEST") # Prints concatenated string
+
---
+++ beginner/notes.md
@@ -0,0 +1,6 @@
+1. ```del``` keyword is used for deleting variable references and it would throw NameError when there is a call to a variable after it has been deleted.
+2. Python vs C/C++ Variables
+![   ](./python_vs_c_c++.png)
+
+3. T
+
